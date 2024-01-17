643a3d41fa2d39c4cf95533bc898d9fb90decb0e
Lursy <matheusnrgolden@gmail.com>
2024-01-17 19:38:53 -0300

    update sticker

--- src/commands/utils/sticker.ts
+++ src/commands/utils/sticker.ts
@@ -5,33 +5,6 @@ import ffmpeg from "fluent-ffmpeg";
 import sharp from "sharp";
 
 export class stickerMedia{
-    private insertAttr = async (path: string = '') => {
-      const img = new webp.Image();
-    
-      const json = {
-        "sticker-pack-id": "666",
-        "sticker-pack-name": "Ruby",
-        "sticker-pack-publisher": "BOT"
-      };
-    
-      const exifAttr = Buffer.from([
-        0x49, 0x49, 0x2a, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57,
-        0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00,
-      ]);
-    
-      const jsonBuff = Buffer.from(JSON.stringify(json), "utf-8");
-    
-      const exif = Buffer.concat([exifAttr, jsonBuff]);
-    
-      exif.writeUIntLE(jsonBuff.length, 14, 4);
-      
-      await img.load(path);
-    
-      img.exif = exif;
-    
-      await img.save(path);
-    };
-
     makeStiker = async (mmedia: proto.Message.IVideoMessage | proto.Message.IImageMessage | proto.Message.IDocumentMessage) => {
       let buffer = Buffer.from([]);
       let namePath = "sticker";
